968393f501a5e407f1fa8d1ca72070503f8050c0
Breno Garcia <BrenoGHeat@gmail.com>
2024-12-17 12:20:58 -0300

    create route get in TeamView

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response, Request
+from django.forms.models import model_to_dict
+from teams.models import Team
 
 # Create your views here.
+
+
+class TeamView(APIView):
+    def post(self, request: Request):
+        team = Team.objects.create(**request.data)
+        return Response(model_to_dict(team), 201)
+
+    def get(self, request: Request):
+        teams_dict = [
+            model_to_dict(team)
+            for team in Team.objects.all()
+        ]
+
+        return Response(teams_dict, 201)
